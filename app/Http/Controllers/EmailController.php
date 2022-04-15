0eb70c5ef4316c1ec1fadb8f1f21f8fb70f0d6df
Amirul Faiz <amirulfaiz.work@gmail.com>
2022-04-16 00:45:33 +0800

    Create Job to send Email

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\SendEmailJob;
+// use App\Mail\SendMailable;
 use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Mail;
 
 class EmailController extends Controller
 {
     public function sendEmail()
     {
-        Mail::to('amirulfaiz.work@gmail.com')->send(new SendMailable());
+        // Mail::to('amirulfaiz.work@gmail.com')->send(new SendMailable());
+        // echo "Email Sent.";
+
+        $emailJob = (new SendEmailJob())->delay(now()->addSeconds(3));
+        dispatch($emailJob);
+
         echo "Email Sent.";
     }
 }
